261ff0af4240927d6423efeef121c985e48781cb
jcq3 <jack.mongaillard@gmail.com>
2020-11-17 10:19:27 +0100

    parsing over the api response + minor design refactoring

---
+++ apiwrapper.js
@@ -0,0 +1,12 @@
+/**
+ * API : https://wasabi.i3s.unice.fr/apidoc/
+ * Nota Bene : The API is wrapped with basic js using fetch, thus error handling is not optimal
+ **/
+
+// Search - Get songs by awardName
+async function getSongsByAward(awardName) {
+    // async, await, then servent à forcer le synchronisme (par défaut js asynchrone)
+    let response = await fetch("https://wasabi.i3s.unice.fr/search/award/"+awardName)
+    let data = await response.json()
+    return data;
+}
