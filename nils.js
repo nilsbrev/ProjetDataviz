6c3bc49e85d91829a897823bd13e54c8727f73c1
jcq3 <jack.mongaillard@gmail.com>
2020-11-17 12:18:25 +0100

    minor modification on syntax

--- nils.js
+++ nils.js
@@ -1,10 +1,10 @@
 import { getAlbumsTitlesByAward } from "./modules/fetchdata.mjs";
 
-async function nils(){
+async function main(){
     //getSongsByAward("Platinum").then(data => console.log(data));
 //rund3(getSongsByAward("Platinum"))
 // getAlbumsTitlesByAward(awardName)
     getAlbumsTitlesByAward("Platinum","albumTitle");
 }
 
-nils()
+main()
