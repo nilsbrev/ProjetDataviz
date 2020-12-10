c556ebf99348cfb7c8ac98b34b148d314609ac6d
jcq3 <jack.mongaillard@gmail.com>
2020-12-10 18:39:34 +0100

    graph nils => version ok

--- nils/nils.js
+++ nils/nils.js
@@ -241,7 +241,8 @@ const countries = ["Afghanistan",
     "Zambia",
     "Zimbabwe"
 ];
-var curated = []
+let curated = []
+let clean = []
 countries.forEach(c => {
     let occurence = new Object()
     occurence.country = c
@@ -251,48 +252,159 @@ countries.forEach(c => {
 
 })
 
-var width = 400, height = 400;
+const margin = {top: 20, right: 40, bottom: 60, left: 50},
+    width = 1400 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom,
+    keys = ["female", "male"],
+    colors = ["#7bccc4", "#084081"],
+    legendCellSize = 20,
+    tooltipWidth = 210;
 
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3.select("#chart").append("svg")
+    .attr("id", "svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-draw();
-d3.select("#draw")
-    .on("click", draw);
 
-function draw() {
-    //var data = getData();
-
-}
-var obj = new Object();
-obj.name = "Raj";
-obj.age = 32;
-obj.married = false;
-
-async function main() {
     // Get curated data and count sex by country
-    d3.json("wasabi-artist.json").then(function(data) {
+    d3.json("wasabi-artist.json", function(data) {
         data.forEach(obj => {
             let g = obj.gender
             let c = obj.location.country
-            if (g != '' && c != '')  {
-                for (let i = 0; i <  curated.length; i ++ ){
-                    if (curated[i].country == c){
-                        if (g == "Female")
-                        {
+            if (g != '' && c != '') {
+                for (let i = 0; i < curated.length; i++) {
+                    if (curated[i].country == c) {
+                        if (g == "Female") {
                             curated[i].female++
                         }
-                        if (g == "Male")
-                        {
+                        if (g == "Male") {
                             curated[i].male++
                         }
                     }
                 }
             }
         })
+        for (let i = 0; i < curated.length; i++) {
+            if (curated[i].female + curated[i].male > 100) {
+                clean.push(curated[i])
+            }
+        }
+        console.log(clean)
+// Transpose the data into layers
+        var dataset = d3.layout.stack()(["female","male"].map(function(sex) {
+            return clean.map(function(d) {
+                return {x: d.country, y: +d[sex]};
+            });
+        }));
+        console.log(dataset)
+        // Set x, y and colors
+        var x = d3.scale.ordinal()
+            .domain(dataset[0].map(function(d) { return d.x; }))
+            .rangeRoundBands([10, width-10], 0.02);
 
-}
+        var y = d3.scale.linear()
+            .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+            .range([height, 0]);
 
-main()
+        var colors = ["#FFC0CB", "#1E90FF"];
+        // Define and draw axes
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(20)
+            .tickSize(-width, 0, 0)
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height+ ")")
+            .call(xAxis);
+
+        // Create groups for each series, rects for each segment
+        var groups = svg.selectAll("g.cost")
+            .data(dataset)
+            .enter().append("g")
+            .attr("class", "cost")
+            .style("fill", function(d, i) { return colors[i]; });
+
+        var rect = groups.selectAll("rect")
+            .data(function(d) { return d; })
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.x); })
+            .attr("y", function(d) { return y(d.y0 + d.y); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+            .attr("width", x.rangeBand())
+            .on("mouseover", function() { tooltip.style("display", null); })
+            .on("mouseout", function() { tooltip.style("display", "none"); })
+            .on("mousemove", function(d) {
+                var xPosition = d3.mouse(this)[0] - 15;
+                var yPosition = d3.mouse(this)[1] - 25;
+                tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                tooltip.select("text").text(d.y);
+            });
+
+        // Draw legend
+        var legend = svg.selectAll(".legend")
+            .data(colors)
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(8," + i * 19 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", function(d, i) {return colors.slice().reverse()[i];});
+
+        legend.append("text")
+            .attr("x", width)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "start")
+            .text(function(d, i) {
+                switch (i) {
+                    case 0: return "Male";
+                    case 1: return "Female";
+                }
+            });
+
+
+// Prep the tooltip bits, initial display is hidden
+        var tooltip = svg.append("g")
+            .attr("class", "tooltip")
+            .style("display", "none");
+
+        tooltip.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("fill", "white")
+            .style("opacity", 0.5);
+
+        tooltip.append("text")
+            .attr("x", 15)
+            .attr("dy", "1.2em")
+            .style("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("font-weight", "bold");
+
+        dataset.sort(function(a, b) {
+            return d3.descending(a.y, b.y)
+        })
+        x.domain(dataset.map(function(d) {
+            return d.key;
+        }));
+
+    })
+
+
+
